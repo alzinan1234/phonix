e9da0ffaeaafc07c02dd5b5b704569708c4b7432
alzinan1234 <papurohan1100@gmail.com>
2024-10-06 14:31:24 +0600

    add

---
+++ src/components/Page/Home/Components/Blog/BlogPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BlogPage = () => {
+  return (
+    <div>
+      <h1>
+        this is our wonderfull blog page there are manny blogs and bautifull
+        this gs
+      </h1>
+    </div>
+  );
+};
+
+export default BlogPage;
