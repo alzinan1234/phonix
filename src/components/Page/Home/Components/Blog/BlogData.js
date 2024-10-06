4edecde7c4b816b43846a13560143c1a41f83222
alzinan1234 <papurohan1100@gmail.com>
2024-10-06 14:09:48 +0600

    ADD

---
+++ src/components/Page/Home/Components/Blog/BlogData.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BlogData = () => {
+  return (
+    <div>
+      <h1>
+        hellow world my name is zinan my father name is md foffazzol hoassin my
+        mothwer name is farzana yesmin my mother is best mon in the world{" "}
+      </h1>
+    </div>
+  );
+};
+
+export default BlogData;
